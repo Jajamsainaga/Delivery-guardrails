79fad3f250887c48b840f3d604868ab1554f5787
Jajamsainaga <k68508880@gmail.com>
2026-02-03 19:19:37 -0500

    Expose subnet outputs from VPC module

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -7,3 +7,12 @@ output "vpc_cidr" {
   description = "CIDR block of the VPC"
   value       = aws_vpc.this.cidr_block
 }
+output "public_subnet_ids" {
+  description = "Public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
